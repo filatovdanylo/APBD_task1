8949276a8bd6687299b0b606afd99ea4ed102af0
Danylo <s32793@pjwstk.edu.pl>
2026-03-10 17:00:56 +0100

    Average commit. Meaningful commit 1

--- APBD_task1/Program.cs
+++ APBD_task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        List<int> nums = new List<int>() {1, 2, 3, 4, 5, 10, 15};
+        double average = FindAverage(nums);
+        Console.WriteLine($"The average for an array is {average:F2}");
+    }
+    public static double FindAverage(List<int> list)
+    {
+        double sum = 0;
+        foreach (int i in list)
+        {
+            sum += i;
+        }
+        return sum / list.Count;
+    }
+}
